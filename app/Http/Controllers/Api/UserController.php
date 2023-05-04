03354862bc83c379079e0875f7942d62598db451
Corina-Lozan <corina.lozan@ebs-integrator.com>
2023-05-04 14:12:37 +0300

    send push notification

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\PushNotificationRequest;
+use App\Http\Requests\Api\SMSNotificationRequest;
 use App\Http\Requests\Api\StoreUserRequest;
 use App\Models\User;
 use App\Services\Notifications\SMSNotificationService;
@@ -29,14 +30,24 @@ class UserController extends Controller
 
     public function push(User $user, PushNotificationRequest $request)
     {
-        (new UserService($user))->notifyPush($request->validated());
+        if (!$user->push_notifications_token) {
+            return response()->json([
+                'status' => 'Failed to send push. Invalid firebase token',
+            ], Response::HTTP_NO_CONTENT);
+        }
+
+        if (!(new UserService($user))->notifyPush($request->validated())) {
+            return response()->json([
+                'status' => 'Failed to send push.',
+            ], Response::HTTP_BAD_REQUEST);
+        }
 
         return response()->json([
             'status' => 'success',
         ], Response::HTTP_OK);
     }
 
-    public function sms(User $user, PushNotificationRequest $request, SMSNotificationService $notificationService)
+    public function sms(User $user, SMSNotificationRequest $request, SMSNotificationService $notificationService)
     {
         if (!(new UserService($user, $notificationService))->notifySMS($request->validated())) {
 
