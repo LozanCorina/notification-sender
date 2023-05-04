2d6fc564850ac388b7ab4a45574838948bd11fdb
Corina-Lozan <corina.lozan@ebs-integrator.com>
2023-05-04 14:07:36 +0300

    push notification

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,35 +3,59 @@
 namespace App\Services;
 
 use App\Models\User;
+use App\Services\Firebase\FirebaseApi;
 use App\Services\Notifications\NotificationSenderInterface;
+use App\Services\Notifications\PushNotificationService;
 use App\Services\Notifications\SMSNotificationService;
+use NotificationChannels\Fcm\Resources\Notification;
 
 class UserService
 {
     public $user;
     public $sender;
 
-    public function __construct(User $user, NotificationSenderInterface $sender)
+    /**
+     * @param User $user
+     * @param null|NotificationSenderInterface $sender
+     */
+    public function __construct(User $user, NotificationSenderInterface $sender = null)
     {
         $this->user = $user;
         $this->sender = $sender;
     }
 
-
+    /**
+     * @param array $data
+     * @return mixed
+     */
     public function notifyPush(array $data)
     {
-        return $this->sender->send();
-    }
+        $notification = new FirebaseApi();
 
-    public function notifySMS(array $data): bool
-    {
-        $notification = new SMSNotificationService();
-
-        $notification->phone = $this->user->phone;
+        $notification->title = $data['title'];
         $notification->body = $data['body'];
 
-        return $this->sender->send($notification, true);
+        if (!$this->user->notify($notification)) {
+            $this->user->push_notifications_token = null;
+            $this->user->save();
+            return false;
+        }
 
+        return true;
+    }
+
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function notifySMS(array $data): bool
+    {
+        $notification = [
+            'phone' => $this->user->phone,
+            'body' => $data['body']
+        ];
+
+        return $this->sender->send($notification, true);
     }
 
 }
