5f8eb3927de20915ee7dec171115269f156c09a4
Corina-Lozan <corina.lozan@ebs-integrator.com>
2023-05-05 17:02:26 +0300

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->boolean('phone_unreachable')->default(false);
+            $table->string('push_notifications_token')->nullable();
             $table->timestamps();
+
+            $table->unique('phone');
         });
     }
 
