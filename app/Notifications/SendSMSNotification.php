28e61f5f280739f9b2c74539a781f887eabc4a84
Corina-Lozan <corina.lozan@ebs-integrator.com>
2023-05-05 17:38:32 +0300

    refactor sms notification

---
+++ app/Notifications/SendSMSNotification.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\VonageMessage;
+
+class SendSMSNotification extends Notification
+{
+    use Queueable;
+
+    private $message;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['vonage'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toVonage(object $notifiable): VonageMessage
+    {
+        return (new VonageMessage)
+            ->content($this->message);
+    }
+}
