55b4c74a400eb6c9418775e39ad4e7fa49cebc70
Corina-Lozan <corina.lozan@ebs-integrator.com>
2023-05-05 17:05:44 +0300

    update push notifications

--- app/Notifications/SendPushNotification.php
+++ app/Notifications/SendPushNotification.php
@@ -1,25 +1,39 @@
 <?php
 
-namespace App\Services\Notifications;
+namespace App\Notifications;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use NotificationChannels\Fcm\FcmChannel;
 use NotificationChannels\Fcm\FcmMessage;
 
-class PushNotificationService extends Notification
+class SendPushNotification extends Notification
 {
+    use Queueable;
+
     private $notification;
 
+    /**
+     * Create a new notification instance.
+     */
     public function __construct($notification)
     {
         $this->notification = $notification;
     }
 
-    public function via($notifiable)
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
     {
         return [FcmChannel::class];
     }
 
+    /**
+     * Get the push representation of the notification.
+     */
     public function toFcm($notifiable)
     {
         return FcmMessage::create()
